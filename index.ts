304d68109a7d5b7f348452ca5e2b78782699419c
Ricky LeDew <adotout1@gmail.com>
2022-12-12 17:28:13 -0500

    Functional webrtc text chat

--- index.ts
+++ index.ts
@@ -1,23 +1,29 @@
-import express from 'express';
-import { Server, Socket } from 'socket.io';
-import http from 'http'
-import cors from 'cors'
+import express from "express";
+import { Server, Socket } from "socket.io";
+import http from "http";
+import cors from "cors";
 const app = express();
-const server = new http.Server(app)
+const server = new http.Server(app);
 const io = new Server(server);
-app.use(cors())
+app.use(cors());
 
-
-const rooms = new Map<Socket, Socket | null>();
-
-io.on('connection', (socket) => {
-    socket.on('join', (room: string) => {
-
-        socket.join(room);
-        socket.on('offer', (offer: unknown) => {
-            socket.broadcast.in(room).emit('answer', offer) // forwards the offer to everyone except the sender
-        })
-    })
-})
-
-app.listen(4000);
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.on("join", (room: string) => {
+    socket.join(room);
+    socket.on("offer", (offer: unknown) => {
+      console.log("forwarding offer");
+      // forwards the offer to everyone except the sender
+      socket.broadcast.in(room).emit("offer", offer);
+    });
+    socket.on("answer", (answer: unknown) => {
+      // forwards the answer to the other person in the room
+      socket.broadcast.in(room).emit("answer", answer);
+    });
+    socket.on("icecandidate", (icecandidate: unknown) => {
+      // forwards the ICE candidate to the other person in the room
+      socket.broadcast.in(room).emit("icecandidate", icecandidate);
+    });
+  });
+});
+server.listen(4000);
--- webrtc-frontend/src/App.tsx
+++ webrtc-frontend/src/App.tsx
@@ -1,50 +1,201 @@
-import { useEffect, useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useEffect, useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import io from "socket.io-client";
 
-import io from 'socket.io-client';
+import "./App.css";
 
-import './App.css'
-
-const socket = io('ws://localhost:4000', {
-  transports: ['websocket']
+const socket = io("localhost:4000", {
+  transports: ["websocket"],
 });
+
 function App() {
   const roomRef = useRef<HTMLInputElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
   const preRef = useRef<HTMLPreElement>(null);
 
-  useEffect(() => {
-    socket.on('answer', (answer: string) => {
-      console.log("answer: ", answer)
-      if (preRef.current)
-        preRef.current.innerText = answer;
+  const [room, setRoom] = useState<string>("");
+
+  const [peerConn, setPeerConn] = useState(
+    new RTCPeerConnection({
+      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
     })
-  }, [])
+  );
+
+  const [sendChannel, setSendChannel] = useState<RTCDataChannel>();
+  const [recieveChannel, setRecieveChannel] = useState<RTCDataChannel>();
+
+  const [active, setActive] = useState(false);
 
   function join() {
-    console.log('join');
-    socket.emit('join', roomRef.current?.value);
+    setRoom(roomRef.current?.value ?? "");
+    socket.emit("join", roomRef.current?.value);
   }
-  
+
+  // Listen for an offer
+  useEffect(() => {
+    createDataChannels();
+    socket.once("offer", async (offer: RTCSessionDescriptionInit) => {
+      // We're answering, noone else should answer
+      socket.off("answer");
+      const remoteDesc = new RTCSessionDescription(offer);
+      peerConn.setRemoteDescription(remoteDesc);
+
+      if (preRef.current) preRef.current.innerText += `Offer Recieved.\n`;
+
+      // After offer is recieved, send an answer
+      const answer = await peerConn.createAnswer();
+      const localDesc = new RTCSessionDescription(answer);
+      await peerConn.setLocalDescription(localDesc);
+
+      socket.emit("answer", answer);
+
+      if (preRef.current) preRef.current.innerText += `Sent Answer.\n`;
+
+      setUpICENegotiation();
+
+      waitUntilConnected();
+    });
+  }, []);
+
+  async function sendOffer() {
+    // We're initiating, don't listen to other offers
+    socket.off("offer");
+    // Sends an offer to the other peer
+    const offer = await peerConn.createOffer();
+    const desc = new RTCSessionDescription(offer);
+    await peerConn.setLocalDescription(desc);
+
+    socket.emit("offer", offer);
+
+    if (preRef.current) preRef.current.innerText += "Sent offer.\n";
+
+    // Listens for the answer
+    socket.once("answer", (answer: RTCSessionDescriptionInit) => {
+      const desc = new RTCSessionDescription(answer);
+      peerConn.setRemoteDescription(desc);
+
+      if (preRef.current) preRef.current.innerText += `Answer Recieved.\n`;
+
+      setUpICENegotiation();
+
+      waitUntilConnected();
+    });
+  }
+
+  function setUpICENegotiation() {
+    if (preRef.current)
+      preRef.current.innerText += `Setting up ICE candidate exchange...\n`;
+
+    peerConn.addEventListener("icecandidateerror", (e) => {
+      if (preRef.current)
+        preRef.current.innerText += `Error in ICE candidate exchange.\n`;
+    });
+    // If we find any, send out ICE candidates
+    peerConn.addEventListener("icecandidate", (e) => {
+      console.log("Send cand ", e.candidate);
+
+      if (!e.candidate) {
+        socket.emit("icecandidate", null);
+        if (preRef.current)
+          preRef.current.innerText += `End of ICE candidates.\n`;
+      } else {
+        if (preRef.current)
+          preRef.current.innerText += `Found new ICE candidate ${e.candidate?.address}, sending.\n`;
+
+        const sentCandidate: RTCIceCandidateInit = {
+          candidate: e.candidate.candidate,
+          sdpMid: e.candidate.sdpMid,
+          sdpMLineIndex: e.candidate.sdpMLineIndex,
+          usernameFragment: e.candidate.usernameFragment,
+        };
+
+        socket.emit("icecandidate", sentCandidate);
+      }
+    });
+
+    // Listen for incoming candidates
+    socket.on("icecandidate", async (candidate: RTCIceCandidateInit | null) => {
+      console.log("Recv cand ", candidate);
+
+      if (candidate !== null) {
+        if (preRef.current) {
+          preRef.current.innerText += `Recieved ICE candidate. \n`;
+        }
+        await peerConn.addIceCandidate(candidate);
+      } else {
+        await peerConn.addIceCandidate(undefined);
+      }
+    });
+  }
+
+  function waitUntilConnected() {
+    peerConn.addEventListener("connectionstatechange", (e) => {
+      if (peerConn.connectionState == "connected") {
+        setActive(true);
+        if (preRef.current)
+          preRef.current.innerText += "Successfully connected!\n";
+      }
+    });
+  }
+
+  function createDataChannels() {
+    const channel = peerConn.createDataChannel("messages");
+    setSendChannel(channel);
+
+    peerConn.addEventListener("datachannel", (e) => {
+      e.channel.addEventListener("message", (e) => {
+        if (preRef.current) preRef.current.innerText += `> ${e.data}\n`;
+      });
+      setRecieveChannel(e.channel);
+    });
+  }
+
   function sendMsg() {
-    console.log('offer');
-    socket.emit('offer', inputRef.current?.value);
+    console.log("sending message");
+    if (sendChannel && inputRef.current) {
+      sendChannel.send(inputRef.current.value);
+      inputRef.current.value = "";
+      inputRef.current.focus();
+    }
   }
 
   return (
     <div className="App">
-      <p>join</p>
+      {room && <h1>In room {room}</h1>}
+      <p>Room</p>
       <input ref={roomRef} />
-      <button onSubmit={join}>Join</button>
-
+      <button onClick={join}>Join</button>
+      <br />
+      <br />
+      <button
+        onClick={sendOffer}
+        disabled={active}
+        style={{ backgroundColor: "green", width: "100%" }}
+      >
+        Send Offer
+      </button>
       <br />
 
       <p>Message</p>
-      <input ref={inputRef}></input>
-      <button onSubmit={sendMsg}>Send</button>
-      <pre ref={preRef}></pre>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          return 0;
+        }}
+      >
+        <input ref={inputRef} disabled={!active}></input>
+        <button
+          onClick={() => {
+            sendMsg();
+          }}
+          disabled={!active}
+        >
+          Send
+        </button>
+      </form>
+      <pre ref={preRef} style={{ backgroundColor: "gray" }}></pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
